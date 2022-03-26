00a251baa8095f34be6bb1821c74b611e0359439
DaniBenavi <danibenavi1907@gmail.com>
2022-03-25 14:35:32 -1200

    Nuevos cambios

--- login.php
+++ login.php
@@ -6,23 +6,22 @@ if (isset($_POST['login'])) {
     $username = $_POST['usuario'];
     $password = $_POST['password'];
 
-    $query = $conn->prepare("SELECT * FROM usuarios WHERE username = :username and password=password");
-    $query->bindParam("username", $username, PDO::PARAM_STR);
+    $query = $conn->prepare("SELECT * FROM usuarios WHERE username = :username and password=:password");
+    $query->bindParam("username", $username,  PDO::PARAM_STR);
     $query->execute();
 
     $result = $query->fetch(PDO::FETCH_ASSOC);
     if (!$result) {
         echo '¡La combinación de nombre de usuario y contraseña es incorrecta!';
     } else {
-        if ($password= $result['password'] && $username = $result['username']) {
+        if ($password = $result['password'] && $username = $result['username']) {
             echo '¡Felicitaciones, estás registrado!';
-            echo $username. " ";
+            echo $username . " ";
             echo $password;
         } else {
             echo '¡La combinación de nombre de usuario y contraseña es incorrecta!';
         }
     }
-}
-else{
-echo 'rellene los campos';
+} else {
+    echo 'rellene los campos';
 }
