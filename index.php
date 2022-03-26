8bcbd3bbbe386c175b5753db3148f476c46f8ff9
DaniBenavi <danibenavi1907@gmail.com>
2022-03-25 14:32:11 -1200

    Nuevos cambios

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <?php require_once "menu.php" ?>
-  <title>Estructuras de Control</title>
+  <title>Login</title>
 </head>
 
 <body>
@@ -22,11 +22,11 @@
         <form method="post" action="login.php">
           <div class="form-group">
             <label for="usuario">Usuario</label>
-            <input id="usuario" class="form-control" type="text" name="usuario">
+            <input id="usuario" class="form-control" type="text" name="usuario" required>
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input id="password" class="form-control" type="password" name="password">
+            <input id="password" class="form-control" type="password" name="password" required>
           </div><br>
           <a href="new_users.php" class="btn btn-secondary">Registrate</a>
           <button class="btn btn-primary" name="login" type="submit">Entrar</button>
--- login.php
+++ login.php
@@ -2,20 +2,27 @@
 include('config.php');
 session_start();
 
-$username = $_POST['usuario'];
-$password = $_POST['password'];
+if (isset($_POST['login'])) {
+    $username = $_POST['usuario'];
+    $password = $_POST['password'];
 
-$query = $conn->prepare("SELECT * FROM usuarios WHERE username = :username");
-$query->bindParam("username", $username, PDO::PARAM_STR);
-$query->execute();
+    $query = $conn->prepare("SELECT * FROM usuarios WHERE username = :username and password=password");
+    $query->bindParam("username", $username, PDO::PARAM_STR);
+    $query->execute();
 
-$result = $query->fetch(PDO::FETCH_ASSOC);
-if (!$result) {
-    echo '¡La combinación de nombre de usuario y contraseña es incorrecta!';
-} else {
-    if (password_verify($password, $result['password'])) {
-        echo '¡Felicitaciones, estás registrado!';
-    } else {
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+    if (!$result) {
         echo '¡La combinación de nombre de usuario y contraseña es incorrecta!';
+    } else {
+        if ($password= $result['password'] && $username = $result['username']) {
+            echo '¡Felicitaciones, estás registrado!';
+            echo $username. " ";
+            echo $password;
+        } else {
+            echo '¡La combinación de nombre de usuario y contraseña es incorrecta!';
+        }
     }
+}
+else{
+echo 'rellene los campos';
 }
