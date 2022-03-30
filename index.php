61e3deaa4fcaea5e1e97589a8017336679a02f95
DaniBenavi <danibenavi1907@gmail.com>
2022-03-29 20:12:52 -1200

    Nuevos cambios

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <?php require_once "menu.php" ?>
-  <title>Login</title>
+  <?php require_once "navbar.php" ?>
+  <title>LOGIN</title>
 </head>
 
 <body>
@@ -26,7 +26,7 @@
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input id="password" class="form-control" type="password" name="password" required>
+            <input id="password" class="form-control" type="password" name="password">
           </div><br>
           <a href="new_users.php" class="btn btn-secondary">Registrate</a>
           <button class="btn btn-primary" name="login" type="submit">Entrar</button>
--- login.php
+++ login.php
@@ -1,27 +1,24 @@
 <?php
 include('config.php');
 session_start();
-
 if (isset($_POST['login'])) {
+
     $username = $_POST['usuario'];
     $password = $_POST['password'];
 
-    $query = $conn->prepare("SELECT * FROM usuarios WHERE username = :username and password=:password");
-    $query->bindParam("username", $username,  PDO::PARAM_STR);
+    $query = $conn->prepare("SELECT * FROM usuarios WHERE username=:username");
+    $query->bindParam("username", $username, PDO::PARAM_STR);
     $query->execute();
-
     $result = $query->fetch(PDO::FETCH_ASSOC);
+
     if (!$result) {
-        echo '¡La combinación de nombre de usuario y contraseña es incorrecta!';
+        header("location: index.php");
     } else {
-        if ($password = $result['password'] && $username = $result['username']) {
-            echo '¡Felicitaciones, estás registrado!';
-            echo $username . " ";
-            echo $password;
+        if (password_verify($password, $result['password'])) {
+            $_SESSION['user_id'] = $result['id'];
+            header("location: principal.php");
         } else {
-            echo '¡La combinación de nombre de usuario y contraseña es incorrecta!';
+            header("location: index.php");
         }
     }
-} else {
-    echo 'rellene los campos';
 }
